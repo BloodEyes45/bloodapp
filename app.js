d71e816b43e3cddf660f24a94e497c27d601f00a
BloodEyes1 <garenimsilik45@gmail.com>
2026-09-18 01:55:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -358,7 +358,7 @@ async function handleAiMessage() {
 
     try {
         // Gerçek Gemini API İsteği (Public Gemini API Endpoint)
-        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSy... (API Anahtarını buraya ekleyebilirsin)`, {
+        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AQ.Ab8RN6Jp7Jc2Mgw-n2EscaOAuochEjiiGsK5chKieLYyDjL8lA`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
